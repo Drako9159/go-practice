21e43d487a7dec9d9738b7aa48258b4d27a58fef
drako9159 <chochito9159@gmail.com>
2024-04-21 19:04:28 -0600

    feat(explain): add explain

---
+++ explain/default-values.go
@@ -0,0 +1,13 @@
+
+
+
+// Default values
+
+bool: false
+string: ""
+int: 0
+array: nil
+map: nil
+slice: nil
+pointer: nil
+function: nil
---
+++ explain/loop.go
@@ -0,0 +1,9 @@
+// Loop Range
+
+var arr = []int{5,4,3,2,1}
+
+for i, v := range arr {
+	fmt.Println("index: %d, value: %d", i, v)
+}
+
+// Range is for slices, arrays, maps and strings
