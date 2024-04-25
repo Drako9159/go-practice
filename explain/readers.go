2f556e85c85ae2b2a20211982569226ce5002767
drako9159 <chochito9159@gmail.com>
2024-04-25 16:21:08 -0600

    feat(images): example for create images

--- explain/readers.go
+++ explain/readers.go
@@ -53,3 +53,34 @@ func main() {
 	io.Copy(os.Stdout, &r) // You cracked the code!
 }
 
+// Images
+
+func main () {
+	// create image RGBA 100x100 pixels, color black with red point
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+
+	// draw black image
+	for x:= 0; x < 100; x++ {
+		for y:= 0; y < 100; y++ {
+			img.Set(x, y, color.RGBA{0, 0, 0, 255})
+		}
+	} 
+
+	// draw red point
+	img.Set(50, 50, color.RGBA{255, 0, 0, 255})
+
+	// save image
+	f, err := os.Create("image.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = png.Encode(f, img)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("image saved")
+	f.Close()
+	defer f.Close()
+
+	
+}
