82cf2146266c12369b0dc73c0af48404ea43d5c8
drako9159 <chochito9159@gmail.com>
2024-04-27 19:22:14 -0600

    feat(channels): example channels

--- explain/routines.go
+++ explain/routines.go
@@ -18,3 +18,50 @@ func main() {
 	go say("world")
 	say("hello")
 }
+
+
+// Channels
+
+ch := make(chan int)
+
+// channel communication that transport int values
+
+func say(s string, ch chan string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(s)
+		ch <- s
+	}
+}
+
+func main() {
+	ch := make(chan string)
+	go say("Hello", ch)
+
+	for {
+		msg := <-ch
+		fmt.Println(msg)
+	}
+
+	fmt.Println("Goodbye")
+}
+
+// Buffered channels
+
+ch := make(chan int, 100)
+
+// channel with capacity of 100
+
+func main(){
+	ch := make(chan int, 2)
+
+	ch <- 1
+	ch <- 2
+	ch <- 3 // fatal error: all goroutines are asleep - deadlock!
+
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
+
+// Range and close
+
