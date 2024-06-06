2b1fd701514050eac78a7b6f6287ed5b27ca088b
drako9159 <chochito9159@gmail.com>
2024-06-06 06:42:53 -0600

    feat(logs): ui logs

--- project/cmd/web/routes/log.route.go
+++ project/cmd/web/routes/log.route.go
@@ -1,9 +1,20 @@
 package routes
 
+import (
+	"GoBaby/internals/models"
+	"GoBaby/internals/utils"
+	logDomain "GoBaby/cmd/web/domain/log"
+	"net/http"
+	"GoBaby/ui"
+)
 
 
 func LogRender() {
 
-	mux.HandlerFunc("GET "+models.RoutesInstance.LOGS, logDomain)
+	mux.HandlerFunc("GET "+models.RoutesInstance.LOGS, logDomain.LogView)
 }
 
+func LogTable(w http.ResponseWriter, r *http.Request) {
+	utils.CheckIfPath(w, r, models.RoutesInstance.LOG_TABLE)
+	utils.ParseTemplateFiles(w, "logTable", utils.EmptyStruct, utils.EmptyFuncMap, ui.Content, "html/pages/logs/logTable.tmpl.html")
+}
